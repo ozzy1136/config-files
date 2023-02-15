c3a5490ff3b4a0259acf64ba5ceaab951cc6865e
Ozmar Mendoza <101409227+ozzy1136@users.noreply.github.com>
2023-02-14 19:20:34 -0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,51 +1,26 @@
+const prod = process.env.NODE_ENV === "production";
+
 module.exports = {
-	plugins:
-		process.env.NODE_ENV === "production"
-			? {
-					"postcss-preset-env": {
-						autoprefixer: true,
-						stage: 2,
-						features: {
-							"nesting-rules": true,
-							"custom-media-queries": {
-								importFrom: "global.css",
-							},
-							"all-property": false,
-							"any-link-pseudo-class": false,
-							"custom-properties": false,
-							"font-variant-property": false,
-							"image-set-function": false,
-							"is-pseudo-class": false,
-							"logical-properties-and-values": false,
-							"not-pseudo-class": false,
-							"overflow-property": false,
-							"overflow-wrap-property": false,
-							"unset-value": false,
-						},
-					},
-			  }
-			: {
-					"postcss-preset-env": {
-						autoprefixer: true,
-						stage: 2,
-						features: {
-							"nesting-rules": true,
-							"custom-media-queries": {
-								importFrom: "global.css",
-							},
-							"all-property": false,
-							"any-link-pseudo-class": false,
-							"custom-properties": false,
-							"font-variant-property": false,
-							"gap-properties": false,
-							"image-set-function": false,
-							"is-pseudo-class": false,
-							"logical-properties-and-values": false,
-							"not-pseudo-class": false,
-							"overflow-property": false,
-							"overflow-wrap-property": false,
-							"unset-value": false,
-						},
-					},
-			  },
+	plugins: {
+		"postcss-preset-env": {
+			autoprefixer: prod ? true : false,
+			stage: 2,
+			features: {
+				"nesting-rules": true,
+				"custom-media-queries": true,
+				"all-property": false,
+				"any-link-pseudo-class": false,
+				"custom-properties": false,
+				"font-variant-property": false,
+				"gap-properties": prod ? true : false,
+				"image-set-function": false,
+				"is-pseudo-class": false,
+				"logical-properties-and-values": false,
+				"not-pseudo-class": false,
+				"overflow-property": false,
+				"overflow-wrap-property": false,
+				"unset-value": false,
+			},
+		},
+	}
 };
